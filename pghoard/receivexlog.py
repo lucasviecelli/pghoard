7fbdd7bd5fca501fd54d0e78a5636b78c96083df
Nick Farrell <nick.farrell@aiven.io>
2022-05-26 11:05:18 +1000

    Ensure receivexlog respects new configuration values after SIGHUP

--- pghoard/receivexlog.py
+++ pghoard/receivexlog.py
@@ -12,6 +12,7 @@ import select
 import signal
 import subprocess
 import time
+from queue import Empty, Queue
 
 from .common import (PGHoardThread, set_subprocess_stdout_and_stderr_nonblocking, terminate_subprocess)
 
@@ -19,14 +20,11 @@ from .common import (PGHoardThread, set_subprocess_stdout_and_stderr_nonblocking
 class PGReceiveXLog(PGHoardThread):
     def __init__(self, config, connection_string, wal_location, site, slot, pg_version_server):
         super().__init__()
-        pg_receivexlog_config = config["backup_sites"][site]["pg_receivexlog"]
         self.log = logging.getLogger("PGReceiveXLog")
+        self.new_config = Queue()
         self.config = config
         self.connection_string = connection_string
-        self.disk_space_check_interval = pg_receivexlog_config["disk_space_check_interval"]
         self.last_disk_space_check = time.monotonic()
-        self.min_disk_space = pg_receivexlog_config.get("min_disk_free_bytes")
-        self.resume_multiplier = pg_receivexlog_config["resume_multiplier"]
         self.wal_location = wal_location
         self.site = site
         self.slot = slot
@@ -36,6 +34,22 @@ class PGReceiveXLog(PGHoardThread):
         self.running = False
         self.latest_activity = datetime.datetime.utcnow()
         self.log.debug("Initialized PGReceiveXLog")
+
+    @property
+    def pg_receivexlog_config(self):
+        return self.config["backup_sites"][self.site]["pg_receivexlog"]
+
+    @property
+    def disk_space_check_interval(self):
+        return self.pg_receivexlog_config["disk_space_check_interval"]
+
+    @property
+    def min_disk_space(self):
+        return self.pg_receivexlog_config.get("min_disk_free_bytes")
+
+    @property
+    def resume_multiplier(self):
+        return self.pg_receivexlog_config["resume_multiplier"]
 
     def run_safe(self):
         self.running = True
@@ -60,7 +74,17 @@ class PGReceiveXLog(PGHoardThread):
         self.pid = proc.pid
         self.log.info("Started: %r, running as PID: %r", command, self.pid)
         while self.running:
-            rlist, _, _ = select.select([proc.stdout, proc.stderr], [], [], min(1.0, self.disk_space_check_interval))
+            try:
+                self.config = self.new_config.get(block=False)
+                self.log.info("Loaded new configuration")
+                self.log.info(str(self.pg_receivexlog_config))
+                continue
+            except Empty:
+                pass
+
+            rlist, _, _ = select.select([proc.stdout, proc.stderr], [], [],
+                                        min(30, max(1.0, self.disk_space_check_interval)))
+            self.log.info(str(rlist))
             for fd in rlist:
                 content = fd.read()
                 if content:
@@ -71,7 +95,12 @@ class PGReceiveXLog(PGHoardThread):
             self.stop_or_continue_based_on_free_disk()
         self.continue_pg_receivewal()
         rc = terminate_subprocess(proc, log=self.log)
-        self.log.debug("Ran: %r, took: %.3fs to run, returncode: %r", command, time.time() - start_time, rc)
+        self.log.debug(
+            "Ran: %r, took: %.3fs to run, returncode: %r",
+            command,
+            time.time() - start_time,
+            rc,
+        )
         self.running = False
 
     def stop_or_continue_based_on_free_disk(self):
@@ -95,7 +124,8 @@ class PGReceiveXLog(PGHoardThread):
             if bytes_free >= min_free_bytes:
                 self.log.info(
                     "Free disk space %.1f MiB is above configured resume threshold %.1f MiB, resuming pg_receive(wal|xlog)",
-                    bytes_free / 1024.0 / 1024.0, min_free_bytes / 1024.0 / 1024.0
+                    bytes_free / 1024.0 / 1024.0,
+                    min_free_bytes / 1024.0 / 1024.0,
                 )
                 self.continue_pg_receivewal()
 
